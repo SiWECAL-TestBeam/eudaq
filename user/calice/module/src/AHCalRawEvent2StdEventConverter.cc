e125174a569e048227ffbc75f1ddf0ebb562a5b2
Jiri Kvasnicka <jiri.kvasnicka@desy.de>
2017-11-09 17:30:52 +0100

    big layers in the online display. Still needs to recompile when layer/asic  numbers change

--- user/calice/module/src/AHCalRawEvent2StdEventConverter.cc
+++ user/calice/module/src/AHCalRawEvent2StdEventConverter.cc
@@ -5,6 +5,9 @@
 #include <array>
 #endif
 
+#define planesXsize 24
+#define planesYsize 24
+
 class AHCalRawEvent2StdEventConverter: public eudaq::StdEventConverter {
    public:
       bool Converting(eudaq::EventSPC d1, eudaq::StdEventSP d2, eudaq::ConfigSPC conf) const override;
@@ -12,8 +15,62 @@ class AHCalRawEvent2StdEventConverter: public eudaq::StdEventConverter {
 
    private:
       int getPlaneNumberFromCHIPID(int chipid) const;
-      int getXcoordFromCHIPID(int chipid, int channelNr) const;
-      int getYcoordFromCHIPID(int chipid, int channelNr) const;
+      int getXcoordFromChipChannel(int chipid, int channelNr) const;
+      int getYcoordFromChipChannel(int chipid, int channelNr) const;
+      const std::map<int, std::tuple<int, int, int>> mapping = { //tuple: layer, xcoordinate, ycoordinate
+            //layer 1: single HBU
+                  { 185, std::make_tuple(0, 18, 18) },
+                  { 186, std::make_tuple(0, 18, 12) },
+                  { 187, std::make_tuple(0, 12, 18) },
+                  { 188, std::make_tuple(0, 12, 12) },
+                  //layer 10: former layer 12 - full HBU
+                  { 169, std::make_tuple(1, 18, 18) },
+                  { 170, std::make_tuple(1, 18, 12) },
+                  { 171, std::make_tuple(1, 12, 18) },
+                  { 172, std::make_tuple(1, 12, 12) },
+                  { 173, std::make_tuple(1, 18, 6) },
+                  { 174, std::make_tuple(1, 18, 0) },
+                  { 175, std::make_tuple(1, 12, 6) },
+                  { 176, std::make_tuple(1, 12, 0) },
+                  { 177, std::make_tuple(1, 6, 18) },
+                  { 178, std::make_tuple(1, 6, 12) },
+                  { 179, std::make_tuple(1, 0, 18) },
+                  { 180, std::make_tuple(1, 0, 12) },
+                  { 181, std::make_tuple(1, 6, 6) },
+                  { 182, std::make_tuple(1, 6, 0) },
+                  { 183, std::make_tuple(1, 0, 6) },
+                  { 184, std::make_tuple(1, 0, 0) }
+            };
+      const int planeCount = 2;
+      //      const int asicXcoor[256] = {
+//            //layer12
+//            [169]=18, [170]=18, [171]=12, [172]=12,
+//            [173]=18, [174]=18, [175]=12, [176]=12,
+//            [177]=6, [178]=6, [179]=0, [180]=0,
+//            [181]=6, [182]=6, [183]=0, [184]=0,
+//            //singleHBU layer
+//            [185]=18, [186]=18, [187]=12, [188]=12 };
+//      const int asicYcoor[256] = {
+//            //layer12
+//            [169]=18, [170]=12, [171]=18, [172]=12,
+//            [173]=6, [174]=0, [175]=6, [176]=0,
+//            [177]=18, [178]=12, [179]=18, [180]=12,
+//            [181]=6, [182]=0, [183]=6, [184]=0,
+//            //singleHBU layer
+//            [185]=18, [186]=12, [187]=18, [188]=12
+//      };
+//      const int planeNumbers[256] = {
+//            //layer12
+//            [169]=1, [170]=1, [171]=1, [172]=1, [173]=1, [174]=1, [175]=1, [176]=1,
+//            [177]=1, [178]=1, [179]=1, [180]=1, [181]=1, [182]=1, [183]=1, [184]=1,
+//
+//            //singleHBU layer
+//            [185]=0, [186]=0, [187]=0, [188]=0
+//      };
+//      const int planesXsize = 24;
+//      const int planesYsize = 24;
+
+      const int pedestalLimit = 200;
 };
 
 namespace {
@@ -23,14 +80,13 @@ auto dummy0 = eudaq::Factory<eudaq::StdEventConverter>::
 
 bool AHCalRawEvent2StdEventConverter::Converting(eudaq::EventSPC d1, eudaq::StdEventSP d2, eudaq::ConfigSPC conf) const {
    std::string sensortype = "Calice"; //TODO ?? "HBU"
-   const int planeCount = 3;
    auto ev = std::dynamic_pointer_cast<const eudaq::RawEvent>(d1);
    size_t nblocks = ev->NumBlocks();
    std::vector<std::unique_ptr<eudaq::StandardPlane>> planes;
    std::vector<int> HBUHits;
-   std::vector<std::array<int, 144>> HBUs;         //HBU(aka plane) index, x*12+y
+   std::vector<std::array<int, planesXsize * planesYsize>> HBUs;         //HBU(aka plane) index, x*12+y
    for (int i = 0; i < planeCount; ++i) {
-      std::array<int, 144> HBU;
+      std::array<int, planesXsize * planesYsize> HBU;
       HBU.fill(-1); //fill all channels to -1
       HBUs.push_back(HBU); //add the HBU to the HBU
       HBUHits.push_back(0);
@@ -64,7 +120,8 @@ bool AHCalRawEvent2StdEventConverter::Converting(eudaq::EventSPC d1, eudaq::StdE
          int hitbit = (data[5 + data[4] + ichan] & 0x1000) ? 1 : 0;  //extract hitbit
          if (planeNumber >= 0) {  //plane, which is not found, has index -1
             if (hitbit) {
-               int coordIndex = getXcoordFromCHIPID(chipid, ichan) * 12 + getYcoordFromCHIPID(chipid, ichan);  //get the index from the HBU array
+               if (adc < pedestalLimit) continue;
+               int coordIndex = getXcoordFromChipChannel(chipid, ichan) * planesXsize + getYcoordFromChipChannel(chipid, ichan); //get the index from the HBU array
                if (HBUs[planeNumber][coordIndex] >= 0) std::cout << "ERROR: channel already has a value" << std::endl;
                HBUs[planeNumber][coordIndex] = gainbit ? adc : 10 * adc;
                //HBUs[planeNumber][coordIndex] = 1;
@@ -79,11 +136,11 @@ bool AHCalRawEvent2StdEventConverter::Converting(eudaq::EventSPC d1, eudaq::StdE
       std::unique_ptr<eudaq::StandardPlane> plane(new eudaq::StandardPlane(i, "CaliceObject", sensortype));
       //plane->SetSizeRaw(13, 13, 1, 0);
       int pixindex = 0;
-      plane->SetSizeZS(12, 12, HBUHits[i], 1, 0);
-      for (int x = 0; x < 12; x++) {
-         for (int y = 0; y < 12; y++) {
-            if (HBUs[i][x * 12 + y] >= 0) {
-               plane->SetPixel(pixindex++, x, y, HBUs[i][x * 12 + y]);
+      plane->SetSizeZS(planesXsize, planesYsize, HBUHits[i], 1, 0);
+      for (int x = 0; x < planesXsize; x++) {
+         for (int y = 0; y < planesYsize; y++) {
+            if (HBUs[i][x * planesXsize + y] >= 0) {
+               plane->SetPixel(pixindex++, x, y, HBUs[i][x * planesXsize + y]);
             }
          }
       }
@@ -99,21 +156,27 @@ bool AHCalRawEvent2StdEventConverter::Converting(eudaq::EventSPC d1, eudaq::StdE
 }
 
 int AHCalRawEvent2StdEventConverter::getPlaneNumberFromCHIPID(int chipid) const {
+   auto searchIterator = mapping.find(chipid);
+   if (searchIterator == mapping.end()) return -1;
+   auto result = std::get<0>(searchIterator->second);
+   return result;
+//   return planeNumbers[chipid];
+
    int planeNumber = -1;
    switch ((chipid - 1) >> 2) {
-   // case 48: //193
-   //    planeNumber = 0;
-   //    break;
-   case 46://185
-      planeNumber = 0;
-      break;
-   case 42://169
-      planeNumber = 1;
-      break;
-   case 43://173
-      planeNumber = 2;
-      break;
-      
+      // case 48: //193
+      //    planeNumber = 0;
+      //    break;
+      case 46:   //185
+         planeNumber = 0;
+         break;
+      case 42:   //169
+         planeNumber = 1;
+         break;
+      case 43:   //173
+         planeNumber = 2;
+         break;
+
          // case 59: //237
          //    planeNumber = 0;
          //    break;
@@ -156,23 +219,33 @@ int AHCalRawEvent2StdEventConverter::getPlaneNumberFromCHIPID(int chipid) const 
    return planeNumber;
 }
 
-int AHCalRawEvent2StdEventConverter::getXcoordFromCHIPID(int chipid, int channelNr) const {
-   int subx = channelNr / 6;
-   if (((chipid & 0x03) == 0x01) || ((chipid & 0x03) == 0x02)) {
-      //1st and 2nd spiroc are in the righ half of HBU
-      subx += 6;
-   }
+int AHCalRawEvent2StdEventConverter::getXcoordFromChipChannel(int chipid, int channelNr) const {
+   auto searchIterator = mapping.find(chipid);
+   if (searchIterator == mapping.end()) return 0;
+   auto asicXCoordBase = std::get<1>(searchIterator->second);
+
+   int subx = channelNr / 6 + asicXCoordBase;
    return subx;
+//   if (((chipid & 0x03) == 0x01) || ((chipid & 0x03) == 0x02)) {
+//      //1st and 2nd spiroc are in the righ half of HBU
+//      subx += 6;
+//   }
+//   return subx;
 }
 
-int AHCalRawEvent2StdEventConverter::getYcoordFromCHIPID(int chipid, int channelNr) const {
+int AHCalRawEvent2StdEventConverter::getYcoordFromChipChannel(int chipid, int channelNr) const {
+   auto searchIterator = mapping.find(chipid);
+   if (searchIterator == mapping.end()) return 0;
+   auto asicYCoordBase = std::get<2>(searchIterator->second);
+
    int suby = channelNr % 6;
    if (((chipid & 0x03) == 0x00) || ((chipid & 0x03) == 0x03)) {
       //3rd and 4th spiroc have different channel order
       suby = 5 - suby;
    }
-   if (((chipid & 0x03) == 0x01) || ((chipid & 0x03) == 0x03)) {
-      suby += 6;
-   }
+   suby += asicYCoordBase;
+//   if (((chipid & 0x03) == 0x01) || ((chipid & 0x03) == 0x03)) {
+//      suby += 6;
+//   }
    return suby;
 }
